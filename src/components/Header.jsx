76815012b39b33c89dfdba957d298ca482a2498a
unknown <sharipov.r@mail.ru>
2022-05-26 23:25:57 +0400

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ const Header = () => {
     return (
         <nav className="green darken-1">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React shop</a>
+                <a href="/" className="brand-logo">React shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/RuslanSharipov63/react-shop" target='_blank' rel="noreferrer">Repo</a></li>
                 </ul>
             </div>
         </nav>
